cf2ac487e1715556a4cbb09742e6904782176266
Slimkin <doc.slimkin.dgi@google.com>
2020-05-29 16:49:03 +0300

    mod person.py

--- person.py
+++ person.py
@@ -10,26 +10,21 @@ class Contact:
 
 
     def __str__(self):
-        data = f'Имя: {self.first_name}\nФамилия: {self.last_name}\nТелефон: {self.phone}'
-        data2 = self.in_favorites()
-        data3 = self.additional_info()
-        return (data2)
-        
-
-    def in_favorites(self):
+        data = [
+            f'Имя: {self.first_name}\nФамилия: {self.last_name}\nТелефон: {self.phone}'
+        ]
         if self.favorites == True:
-            return 'В избранных: да'
+            data.append('В избранных: да')
         else:
-            return 'В избранных: нет'
+            data.append('В избранных: нет')
+        data.append('Дополнительная информация:')
+        for k, v in self.kwargs.items():
+            data.append(f'    {k} - {v}')
+        if not self.kwargs:
+            data.append('   Отсутствует')
+        return '\n'.join(data)
 
-    def additional_info(self):
-        yield 'Дополнительная информация:'
-        if self.kwargs.items() != None:
-            for k, v in self.kwargs.items():
-                return f'    {k} - {v}'
-        else:
-            return 'Отсутствует'
-
+    
 if __name__ == "__main__":
     
     jhon = Contact('Jhon', 'Smith', '+71234567809', telegram='@jhony', email='jhony@smith.com')
